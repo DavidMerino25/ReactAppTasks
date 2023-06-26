80953743d9f2af7b0352f842cf851d98dd9ce98d
DavidMerino25 <deividrocan2508@gmail.com>
2023-06-26 08:44:43 -0600

    add: styles del header y boton de nueva tarea

--- myapp/App.js
+++ myapp/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./screens/HomeScreen";
@@ -9,8 +10,18 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-        <Stack.Screen name="TaskFormScreen" component={TaskFormScreen}/>
+        <Stack.Screen name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            headerStyle: { backgroundColor: '#7371fc' },
+            headerTitleStyle: { color: '#f5efff' },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => console.log('pressed')} >
+                <Text style={{ color: '#f5efff', marginRight: 10 }}>New Task</Text>
+              </TouchableOpacity>
+            )
+          }} />
+        <Stack.Screen name="TaskFormScreen" component={TaskFormScreen} />
       </Stack.Navigator>
     </NavigationContainer>
 
