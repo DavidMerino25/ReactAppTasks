f3dc5d64dc4f5a80a42b5aca6f0269a127dcb74f
DavidMerino25 <deividrocan2508@gmail.com>
2023-06-26 07:38:19 -0600

    add: conexion al backend

--- myapp/screens/HomeScreen.js
+++ myapp/screens/HomeScreen.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { View } from 'react-native'
+import { getTasks } from "../api";
+import TaskList from "../components/TaskList";
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    const [tasks, setTasks] = useState([])
+    const loadTasks = async () => {
+        const data = await getTasks();
+        setTasks(data)
+    }
+    useEffect(() => {
+        loadTasks()
+    }, []);
 
+    return (
+        <View>
+           <TaskList tasks={tasks}/>
+        </View>
+    )
+}
 export default HomeScreen
