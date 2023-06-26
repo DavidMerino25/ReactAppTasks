39606ab14497777f0062702558e6687aa91fab2c
DavidMerino25 <deividrocan2508@gmail.com>
2023-06-25 21:44:55 -0600

    add: rutas de screens

--- myapp/App.js
+++ myapp/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "./screens/HomeScreen";
+import TaskFormScreen from "./screens/TaskFormScreen";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hola Mundo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+        <Stack.Screen name="TaskFormScreen" component={TaskFormScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#02B321',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});  
+export default App;
