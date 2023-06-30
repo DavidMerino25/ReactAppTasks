43ec5d3fce03210e13630d8fd198c82155d28ce1
DavidMerino25 <deividrocan2508@gmail.com>
2023-06-29 21:13:52 -0600

    add: terminando onrefresh y guardado de las tasks

--- myapp/components/TaskList.js
+++ myapp/components/TaskList.js
@@ -1,13 +1,16 @@
-import {FlatList, RefreshControl } from 'react-native'
+import { FlatList, RefreshControl } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import TaskItem from './TaskItem'
 import { getTasks } from "../api";
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const TaskList = () => {
     const [tasks, setTasks] = useState([])
+    const [refresing, setRefresing] = useState(false)
 
     const loadTasks = async () => {
         const data = await getTasks();
+        console.log("loaded");
         setTasks(data)
     }
     useEffect(() => {
@@ -17,16 +20,24 @@ const TaskList = () => {
     const renderItem = ({ item }) => {
         return <TaskItem task={item} />
     }
+    const onRefresh = React.useCallback(async () => {
+        setRefresing(true)
+        await loadTasks()
+        setRefresing(false)
+    })
+
 
     return (
         <FlatList
-            style={{ width: '100%', backgroundColor: '#fff', padding: 10, borderRadius: 8, marginTop: 10, marginBottom: 10, elevation: 5, shadowColor: '#7371fc', shadowOpacity: 0.2, shadowRadius: 1.5, shadowOffset: { width: 0, height: 2 } }}
+            style={{ width: '100%', backgroundColor: '#fff', padding: 10, borderRadius: 8, marginTop: 10, marginBottom: 10}}
             data={tasks}
             keyExtractor={(item) => item.id + ''}
             renderItem={renderItem}
             refreshControl={
                 <RefreshControl
-                    onRefresh={()=> console.log("refreshing")}
+                    refreshing={refresing}
+                    colors={["#9Bd35A"]}
+                    onRefresh={onRefresh}
                 />
             }
         />
